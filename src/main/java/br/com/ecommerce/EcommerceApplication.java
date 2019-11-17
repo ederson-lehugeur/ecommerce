5d86eb79820081ba698a36c26aad97731a9b9b0c
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-11-17 12:03:15 -0300

    Produto e associação muitos para muitos

--- src/main/java/br/com/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/ecommerce/EcommerceApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.ecommerce.domain.Categoria;
+import br.com.ecommerce.domain.Produto;
 import br.com.ecommerce.repositories.CategoriaRepository;
+import br.com.ecommerce.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class EcommerceApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoritaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
@@ -26,7 +31,19 @@ public class EcommerceApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+
 		categoritaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 
 	}
 
