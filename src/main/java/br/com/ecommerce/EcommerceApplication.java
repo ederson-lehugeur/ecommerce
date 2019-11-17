e03e9d7cc79fb8f357606acd1430add7cbaa77dd
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-11-17 19:21:20 -0300

    Estado e cidade

--- src/main/java/br/com/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/ecommerce/EcommerceApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.ecommerce.domain.Categoria;
+import br.com.ecommerce.domain.Cidade;
+import br.com.ecommerce.domain.Estado;
 import br.com.ecommerce.domain.Produto;
 import br.com.ecommerce.repositories.CategoriaRepository;
+import br.com.ecommerce.repositories.CidadeRepository;
+import br.com.ecommerce.repositories.EstadoRepository;
 import br.com.ecommerce.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class EcommerceApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
@@ -44,6 +54,19 @@ public class EcommerceApplication implements CommandLineRunner {
 
 		categoritaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
+
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
+
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
 
 	}
 
