d9a5f056e57f0b1c813a3d53e95b8fbe9aab1933
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-11-30 11:52:39 -0300

    Validação customizada: verificar e-mail já cadastrado na inserção de clientes

--- src/main/java/br/com/ecommerce/dto/ClienteDTO.java
+++ src/main/java/br/com/ecommerce/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.ecommerce.domain.Cliente;
+import br.com.ecommerce.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
