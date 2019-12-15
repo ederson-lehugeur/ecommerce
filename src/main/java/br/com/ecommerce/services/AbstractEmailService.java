e338afa16c5dab0196a0c22e5731431215da40bc
Éderson Lehugeur <eder.lehugeur@gmail.com>
2019-12-15 00:54:46 -0300

    E-mail HTML

--- src/main/java/br/com/ecommerce/services/AbstractEmailService.java
+++ src/main/java/br/com/ecommerce/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package br.com.ecommerce.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import br.com.ecommerce.domain.Pedido;
 
@@ -12,14 +20,31 @@ public abstract class AbstractEmailService implements EmailService {
 	@Value("${default.sender}")
 	private String sender;
 
+	@Autowired
+	private TemplateEngine templateEngine;
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
 	@Override
 	public void sendOrderConfirmationEmail(Pedido pedido) {
 		SimpleMailMessage simpleMailMessage = prepareSimpleMailMessageFromPedido(pedido);
 		sendEmail(simpleMailMessage);
 	}
 
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido pedido) {
+		try {
+			MimeMessage mimeMessage = prepareMimeMessageFromPedido(pedido);
+			sendHtmlEmail(mimeMessage);
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(pedido);
+		}
+	}
+
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido pedido) {
 		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
 		simpleMailMessage.setTo(pedido.getCliente().getEmail());
 		simpleMailMessage.setFrom(sender);
 		simpleMailMessage.setSubject("Pedido confirmado! Código: " + pedido.getId());
@@ -27,5 +52,24 @@ public abstract class AbstractEmailService implements EmailService {
 		simpleMailMessage.setText(pedido.toString());
 
 		return simpleMailMessage;
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido pedido) throws MessagingException {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+		mimeMessageHelper.setTo(pedido.getCliente().getEmail());
+		mimeMessageHelper.setFrom(sender);
+		mimeMessageHelper.setSubject("Pedido confirmado! Código: " + pedido.getId());
+		mimeMessageHelper.setText(htmlFromTemplatePedido(pedido), true);
+
+		return mimeMessage;
+	}
+
+	protected String htmlFromTemplatePedido(Pedido pedido) {
+		Context context = new Context();
+		context.setVariable("pedido", pedido);
+
+		return templateEngine.process("email/confirmacaoPedido", context);
 	}
 }
